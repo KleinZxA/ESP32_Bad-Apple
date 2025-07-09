076487c6c2884215f8f7565c26f7b0d5f894b873
KleinZxA <ranzaf1414@gmail.com>
2025-07-09 19:39:52 +0900

    Added comments

--- ESP32_Bad-Apple.ino
+++ ESP32_Bad-Apple.ino
@@ -1,11 +1,27 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
-#include "oled_frames.h"  // your generated file
+#include "oled_frames.h"
 
+//Screen Width and size
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64 
+
+//I2C declaration
+//If using I2C Comment these 2
 #define OLED_SDA 14
 #define OLED_SCL 12
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
-Adafruit_SSD1306 display(128, 64, &Wire, -1);
+//SPI declaration
+//If Using SPI version Uncomment below
+//#include <SPI.h>
+//#define OLED_MOSI   9
+//#define OLED_CLK   10
+//#define OLED_DC    11
+//#define OLED_CS    12
+//#define OLED_RESET 13
+//Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT,OLED_MOSI, OLED_CLK, OLED_DC, OLED_RESET, OLED_CS);
+
 
 void setup() {
   Wire.begin(OLED_SDA, OLED_SCL);
@@ -19,6 +35,6 @@ void loop() {
     display.clearDisplay();
     display.drawBitmap(0, 0, frames[i], 128, 64, SSD1306_WHITE);
     display.display();
-    delay(73); //
+    delay(73); //Adjust to your specifications (Adjusts Framerate, Although oled_frames.h is 10fps)
   }
 }
